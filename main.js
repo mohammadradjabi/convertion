a8e57f518af185afb367197767750412b1be7051
mohammadradjabi <mohammad.radjabi75@gmail.com>
2023-12-09 22:25:09 +0330

    age2

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ let h1 = document.querySelectorAll("h1");
 
 let shamsiM;
 let shamsiY;
+let shamsiD;
 submit.addEventListener("click", function () {
   h1.forEach((elem) => {
     elem.style.backgroundColor = "rgb(76, 76, 170)";
@@ -20,85 +21,150 @@ submit.addEventListener("click", function () {
     elem.style.padding = "0 5px";
   });
   if (year.value == 1990) {
-    shamsiY = 1368;
+    shamsiY = 33;
   } else if (year.value == 1991) {
-    shamsiY = 1369;
+    shamsiY = 32;
   } else if (year.value == 1992) {
-    shamsiY = 1370;
+    shamsiY = 31;
   } else if (year.value == 1993) {
-    shamsiY = 1371;
+    shamsiY = 30;
   } else if (year.value == 1994) {
-    shamsiY = 1372;
+    shamsiY = 29;
   } else if (year.value == 1995) {
-    shamsiY = 1373;
+    shamsiY = 28;
   } else if (year.value == 1996) {
-    shamsiY = 1374;
+    shamsiY = 27;
   } else if (year.value == 1997) {
-    shamsiY = 1375;
+    shamsiY = 26;
   } else if (year.value == 1998) {
-    shamsiY = 1376;
+    shamsiY = 25;
   } else if (year.value == 1999) {
-    shamsiY = 1377;
+    shamsiY = 24;
   } else if (year.value == 2000) {
-    shamsiY = 1378;
+    shamsiY = 23;
   } else if (year.value == 2001) {
-    shamsiY = 1379;
+    shamsiY = 22;
   } else if (year.value == 2002) {
-    shamsiY = 1380;
+    shamsiY = 21;
   } else if (year.value == 2003) {
-    shamsiY = 1381;
+    shamsiY = 20;
   } else if (year.value == 2004) {
-    shamsiY = 1382;
+    shamsiY = 19;
   } else if (year.value == 2005) {
-    shamsiY = 1383;
+    shamsiY = 18;
   } else if (year.value == 2006) {
-    shamsiY = 1384;
+    shamsiY = 17;
   } else if (year.value == 2007) {
-    shamsiY = 1385;
+    shamsiY = 16;
   } else if (year.value == 2008) {
-    shamsiY = 1386;
+    shamsiY = 15;
   } else if (year.value == 2009) {
-    shamsiY = 1387;
+    shamsiY = 14;
   } else if (year.value == 2010) {
-    shamsiY = 1388;
+    shamsiY = 13;
   } else {
     alert("Choose Date within 1990-2010");
   }
   if (month.value == 1) {
-    shamsiM = 10;
-  } else if (month.value == 2) {
-    shamsiM = 11;
-  } else if (month.value == 3) {
-    shamsiM = 12;
-  } else if (month.value == 4) {
-    shamsiM = 1;
-  } else if (month.value == 5) {
-    shamsiM = 2;
-  } else if (month.value == 6) {
-    shamsiM = 3;
-  } else if (month.value == 7) {
-    shamsiM = 4;
-  } else if (month.value == 8) {
-    shamsiM = 5;
-  } else if (month.value == 9) {
-    shamsiM = 6;
-  } else if (month.value == 10) {
     shamsiM = 7;
-  } else if (month.value == 11) {
+  } else if (month.value == 2) {
     shamsiM = 8;
-  } else if (month.value == 12) {
+  } else if (month.value == 3) {
     shamsiM = 9;
+  } else if (month.value == 4) {
+    shamsiM = 10;
+  } else if (month.value == 5) {
+    shamsiM = 11;
+  } else if (month.value == 6) {
+    shamsiM = 12;
+  } else if (month.value == 7) {
+    shamsiM = 1;
+  } else if (month.value == 8) {
+    shamsiM = 2;
+  } else if (month.value == 9) {
+    shamsiM = 3;
+  } else if (month.value == 10) {
+    shamsiM = 4;
+  } else if (month.value == 11) {
+    shamsiM = 5;
+  } else if (month.value == 12) {
+    shamsiM = 6;
   } else {
     alert("Choose Month within 1-12");
+  }
+  if (day.value == 1) {
+    shamsiD = 30;
+  } else if (day.value == 2) {
+    shamsiD = 29;
+  } else if (day.value == 3) {
+    shamsiD = 28;
+  } else if (day.value == 4) {
+    shamsiD = 27;
+  } else if (day.value == 5) {
+    shamsiD = 26;
+  } else if (day.value == 6) {
+    shamsiD = 25;
+  } else if (day.value == 7) {
+    shamsiD = 24;
+  } else if (day.value == 8) {
+    shamsiD = 23;
+  } else if (day.value == 9) {
+    shamsiD = 22;
+  } else if (day.value == 10) {
+    shamsiD = 21;
+  } else if (day.value == 11) {
+    shamsiD = 20;
+  } else if (day.value == 12) {
+    shamsiD = 19;
+  } else if (day.value == 13) {
+    shamsiD = 18;
+  } else if (day.value == 14) {
+    shamsiD = 17;
+  } else if (day.value == 15) {
+    shamsiD = 16;
+  } else if (day.value == 16) {
+    shamsiD = 15;
+  } else if (day.value == 17) {
+    shamsiD = 14;
+  } else if (day.value == 18) {
+    shamsiD = 13;
+  } else if (day.value == 19) {
+    shamsiD = 12;
+  } else if (day.value == 20) {
+    shamsiD = 11;
+  } else if (day.value == 21) {
+    shamsiD = 10;
+  } else if (day.value == 22) {
+    shamsiD = 9;
+  } else if (day.value == 23) {
+    shamsiD = 8;
+  } else if (day.value == 24) {
+    shamsiD = 7;
+  } else if (day.value == 25) {
+    shamsiD = 6;
+  } else if (day.value == 26) {
+    shamsiD = 5;
+  } else if (day.value == 27) {
+    shamsiD = 4;
+  } else if (day.value == 28) {
+    shamsiD = 3;
+  } else if (day.value == 29) {
+    shamsiD = 2;
+  } else if (day.value == 30) {
+    shamsiD = 1;
+  } else if (day.value == 31) {
+    shamsiD = 31;
+  } else {
+    alert("Choose Day within 1-31");
   }
   document.querySelector("h1.fullname").innerHTML = fullName.value;
   document.querySelector("h1.father").innerHTML = father.value;
   document.querySelector("h1.degree").innerHTML = degree.value;
   document.querySelector("h1.gender").innerHTML = gender.value;
-  document.querySelector("h1.day").innerHTML = day.value;
   document.querySelector("h1.phone").innerHTML = phone.value;
   document.querySelector("h1.email").innerHTML = email.value;
   document.querySelector("h1.city").innerHTML = city.value;
-  document.querySelector("h1.year").innerHTML = shamsiY;
-  document.querySelector("h1.month").innerHTML = shamsiM;
+  document.querySelector("h1.year").innerHTML = `Year: ${shamsiY}`;
+  document.querySelector("h1.day").innerHTML = `Day: ${shamsiD}`;
+  document.querySelector("h1.month").innerHTML = `Month: ${shamsiM}`;
 });
